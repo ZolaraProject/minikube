fb4a48a4a2aaf1e98e8d287307355dcbd7aacd93
Kitae SEO <kitae.seo@epitech.eu>
2025-02-20 16:20:10 +0000

    test update configure cluster

--- configure_cluster.sh
+++ configure_cluster.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+harbor_ca="harbor-ca-cert"
+harbor_ca_name="./harbor.crt"
+
 database_config="postgres-config"
 database_credentials="postgres-credentials"
 
@@ -10,6 +13,12 @@ database_init_file_path="./database/init.sql"
 jwt_secret_key="jwt-secret"
 
 jwt_secret_key_path="./jwtSecretKey"
+
+echo "----------------------------------"
+echo "Copy harbor certificate"
+minikube cp -p zolara $harbor_ca_name /etc/docker/certs.d/nexuszolara.me/ca.crt
+minikube cp -p zolara $harbor_ca_name /etc/containerd/certs.d/nexuszolara.me/ca.crt
+kubectl apply -f kube-daemonset.yaml
 
 echo "----------------------------------"
 echo "Create namespaces"
@@ -25,6 +34,9 @@ kubectl create configmap $database_config --from-file=init.sql=$database_init_fi
 
 echo "----------------------------------"
 echo "Create Secrets"
+# echo "Harbor certificate"
+# kubectl create secret generic $harbor_ca --from-file=$harbor_ca_name -n kube-system
+
 echo "In namespace database"
 kubectl create secret generic $database_credentials --from-file=username=$database_username_path --from-file=password=$database_password_path -n database --dry-run=client -oyaml | kubectl apply -f -
 kubectl create secret generic $database_credentials --from-file=username=$database_username_path --from-file=password=$database_password_path -n vault --dry-run=client -oyaml | kubectl apply -f -
