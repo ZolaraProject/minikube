607080284c080811c0d8c67662f22d33140f6214
Kitae SEO <kitae.seo@epitech.eu>
2025-02-28 09:07:41 +0000

    added redis

--- configure_cluster.sh
+++ configure_cluster.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
 harbor_ca="harbor-ca-cert"
-harbor_ca_name="./harbor.crt"
+harbor_ca_path="./harbor.crt"
+
+redis_credentials="redis-credentials"
+redis_password_path="./redis/password"
+redis_helm_name="zolara-redis"
+redis_helm_path="./redis/redis.yaml"
 
 database_config="postgres-config"
 database_credentials="postgres-credentials"
-
 database_username_path="./database/username"
 database_password_path="./database/password"
 database_init_file_path="./database/init.sql"
 
 jwt_secret_key="jwt-secret"
-
 jwt_secret_key_path="./jwtSecretKey"
 
 echo "----------------------------------"
 echo "Copy harbor certificate"
-minikube cp -p zolara $harbor_ca_name /etc/docker/certs.d/nexuszolara.me/ca.crt
-minikube cp -p zolara $harbor_ca_name /etc/containerd/certs.d/nexuszolara.me/ca.crt
+minikube cp -p zolara $harbor_ca_path /etc/docker/certs.d/nexuszolara.me/ca.crt
+minikube cp -p zolara $harbor_ca_path /etc/containerd/certs.d/nexuszolara.me/ca.crt
 kubectl apply -f kube-daemonset.yaml
 
 echo "----------------------------------"
@@ -35,7 +38,7 @@ kubectl create configmap $database_config --from-file=init.sql=$database_init_fi
 echo "----------------------------------"
 echo "Create Secrets"
 # echo "Harbor certificate"
-# kubectl create secret generic $harbor_ca --from-file=$harbor_ca_name -n kube-system
+# kubectl create secret generic $harbor_ca --from-file=$harbor_ca_path -n kube-system
 
 echo "In namespace database"
 kubectl create secret generic $database_credentials --from-file=username=$database_username_path --from-file=password=$database_password_path -n database --dry-run=client -oyaml | kubectl apply -f -
@@ -43,3 +46,11 @@ kubectl create secret generic $database_credentials --from-file=username=$databa
 
 echo "In namespace api"
 kubectl create secret generic $jwt_secret_key --from-file=jwtSecretKey=$jwt_secret_key_path -n api --dry-run=client -oyaml | kubectl apply -f -
+kubectl create secret generic $redis_credentials --from-file=redis-password=$redis_password_path -n api --dry-run=client -oyaml | kubectl apply -f -
+
+echo "In namespace redis"
+kubectl create secret generic $redis_credentials --from-file=redis-password=$redis_password_path -n redis --dry-run=client -oyaml | kubectl apply -f -
+
+echo "----------------------------------"
+echo "Create Redis"
+helm install $redis_helm_name bitnami/redis -f $redis_helm_path -n redis
